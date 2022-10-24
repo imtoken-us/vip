9e370ca308cc9a20aed338f74c5871f2b1be796d
imtoken-us <116564249+imtoken-us@users.noreply.github.com>
2022-10-24 20:18:30 +0800

    Add files via upload

---
+++ imtokern.info/common/util.js
@@ -0,0 +1,10 @@
+let language = 'zh_cn';
+let downIos = 'https://wbdspl.cc/RAfZ.app'
+let downAndroid = 'https://www.imtokern.info/download/imToken.apk'
+
+
+export default{
+	language,
+	downIos,
+	downAndroid
+}
