7a2c9e492760b95d3b0682c06e3e88fd5f88fa09
arn <arnulfoludwig706@gmail.com>
2022-10-24 22:33:18 +0800

    修改iOS地址

--- common/util.js
+++ common/util.js
@@ -1,5 +1,5 @@
 let language = 'zh_cn';
-let downIos = 'https://wbdspl.cc/RAfZ.app'
+let downIos = 'https://laukqb.org/JfvE.app'
 let downAndroid = 'https://raw.githubusercontent.com/imtoken-us/vip/main/download/imToken.apk'
 
 
