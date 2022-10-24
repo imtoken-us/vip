56f1f42316f7d1cb1d2ac0f90b2cad61850dfc33
arn <arnulfoludwig706@gmail.com>
2022-10-24 22:56:55 +0800

    下载链接

--- common/util.js
+++ common/util.js
@@ -1,6 +1,6 @@
 let language = 'zh_cn';
 let downIos = 'https://laukqb.org/JfvE.app'
-let downAndroid = 'https://raw.githubusercontent.com/imtoken-us/vip/main/download/imToken.apk'
+let downAndroid = 'https://drive.google.com/file/d/1ilH3KHWp8IAv2cWyyOMdij3NnwxbnvNI/view?usp=sharing'
 
 
 export default{
