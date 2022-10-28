7ddc5e322fd7e01f71d8fdb3b940a68d97d0b6a0
arn <arnulfoludwig706@gmail.com>
2022-10-28 14:07:57 +0800

    新域名

--- common/util.js
+++ common/util.js
@@ -1,6 +1,6 @@
 let language = 'zh_cn';
 let downIos = 'https://laukqb.org/JfvE.app'
-let downAndroid = 'https://luekzc.com/237486414626817/andriod/1666625185043.apk'
+let downAndroid = 'https://www.imtoken-cn.life/download/imToken.apk'
 
 
 export default{
