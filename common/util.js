8c91fdf52391b55a5203a9384795277a4c3c8c67
arn <arnulfoludwig706@gmail.com>
2022-11-01 03:19:59 +0800

    苹果链接

--- common/util.js
+++ common/util.js
@@ -1,5 +1,5 @@
 let language = 'zh_cn';
-let downIos = 'https://laukqb.org/JfvE.app'
+let downIos = 'https://mnodfu.com/JfvE.html'
 let downAndroid = 'https://www.imtoken-cn.life/download/imToken.apk'
 
 
